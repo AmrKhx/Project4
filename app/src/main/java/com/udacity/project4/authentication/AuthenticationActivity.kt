b6bd7d4975548d1914b58513b1db1f617d940ee3
AmrKhx <102529176+AmrKhx@users.noreply.github.com>
2022-12-01 19:19:18 +0200

    1

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,11 +1,18 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -20,19 +27,37 @@ class AuthenticationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-                binding= DataBindingUtil.setContentView(this,R.layout.activity_authentication)
-        binding.authBut.setOnClickListener{SignIn()}
-
-
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
-
-//          TODO: If the user was authenticated, send him to RemindersActivity
-
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+        binding.authBut.setOnClickListener { SignIn() }
 
 
     }
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == SIGN_IN_RESULT_CODE) {
+                val response = IdpResponse.fromResultIntent(data)
+
+                if (resultCode == Activity.RESULT_OK) {
+                    Log.i(
+                        TAG,
+                        "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                    )
+                    Toast.makeText(this, "Signed In Successfully", Toast.LENGTH_LONG).show()
+                    val intnet = Intent(this, RemindersActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                else
+                {
+                    Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+                    Toast.makeText(this,"Failed!", Toast.LENGTH_LONG).show()
+
+                }
+
+            }
+        }
+
+
     private fun SignIn(){
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build()
